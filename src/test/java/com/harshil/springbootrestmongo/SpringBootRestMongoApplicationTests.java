824f76a356ef3ad1baff5d02d7af24c476b82cc8
harshilchoudhary <harshilchoudharyids@gmail.com>
2022-04-03 16:54:23 +0530

    PFL-4 Initialized Project

---
+++ src/test/java/com/harshil/springbootrestmongo/SpringBootRestMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshil.springbootrestmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
