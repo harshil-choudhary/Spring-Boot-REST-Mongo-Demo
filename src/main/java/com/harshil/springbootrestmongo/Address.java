3580b12afe9a5cc1028792bd0e7d376d4c0fa42f
harshilchoudhary <harshilchoudharyids@gmail.com>
2022-04-03 17:58:59 +0530

    PFL-4 Basic Data Structure

---
+++ src/main/java/com/harshil/springbootrestmongo/Address.java
@@ -0,0 +1,10 @@
+package com.harshil.springbootrestmongo;
+
+import lombok.Data;
+
+@Data
+public class Address {
+    private String country;
+    private String postCode;
+    private String city;
+}
---
+++ src/main/java/com/harshil/springbootrestmongo/Gender.java
@@ -0,0 +1,5 @@
+package com.harshil.springbootrestmongo;
+
+public enum Gender {
+    MALE, FEMALE
+}
