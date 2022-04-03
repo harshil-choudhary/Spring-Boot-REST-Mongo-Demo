0aefe3d610fc65426cde82f42e84feedd4c009e1
harshilchoudhary <harshilchoudharyids@gmail.com>
2022-04-03 18:21:00 +0530

    PFL-4 Index Creation

--- src/main/java/com/harshil/springbootrestmongo/Student.java
+++ src/main/java/com/harshil/springbootrestmongo/Student.java
@@ -2,6 +2,7 @@ package com.harshil.springbootrestmongo;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -16,6 +17,7 @@ public class Student {
     private String id;
     private String firstName;
     private String lastName;
+    @Indexed(unique = true)        // Will index this field and searches will be equal. The option makes the email unique
     private String email;
     private Gender gender;
     private Address address;
