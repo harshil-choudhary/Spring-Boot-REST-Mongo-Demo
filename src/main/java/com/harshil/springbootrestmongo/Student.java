298977319639d35a13013320c8071b0d72edf864
harshilchoudhary <harshilchoudharyids@gmail.com>
2022-04-03 18:02:16 +0530

    PFL-4 Annotations

--- src/main/java/com/harshil/springbootrestmongo/Student.java
+++ src/main/java/com/harshil/springbootrestmongo/Student.java
@@ -1,13 +1,18 @@
 package com.harshil.springbootrestmongo;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.List;
 
-@Data
+@Data               // Removes the need for constructor, getters and setters
+@Document           // Defines the Document for MongoDB
 public class Student {
+    @Id             // Unique ID for the DB
+    private String id;
     private String firstName;
     private String lastName;
     private String email;
