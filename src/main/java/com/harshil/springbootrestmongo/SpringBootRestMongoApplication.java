b2301929f8b7ac18a40e836fb6b45e4e90398e8f
harshilchoudhary <harshilchoudharyids@gmail.com>
2022-04-03 18:37:56 +0530

    PFL-4 Mongo Template and Query

--- src/main/java/com/harshil/springbootrestmongo/SpringBootRestMongoApplication.java
+++ src/main/java/com/harshil/springbootrestmongo/SpringBootRestMongoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -18,17 +21,18 @@ public class SpringBootRestMongoApplication {
     }
 
     @Bean
-    CommandLineRunner runner(StudentRepository repository) {
+    CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate) {
         return args -> {
             Address address = new Address(
                     "India",
                     "Test",
                     "test"
             );
+            String email = "bruhmoment@bruh.com";
             Student student = new Student(
                     "Harshil",
                     "Choudhary",
-                    "bruhmoment@bruh.com",
+                    email,
                     Gender.MALE,
                     address,
                     List.of("Computer Science", "Math"),
@@ -36,7 +40,20 @@ public class SpringBootRestMongoApplication {
                     LocalDateTime.now()
             );
 
-            repository.insert(student);
+            Query query  = new Query();
+            query.addCriteria(Criteria.where("email").is(email));
+
+            List<Student> students = mongoTemplate.find(query, Student.class);
+
+            if (students.size() > 1) {
+                throw new IllegalStateException("Found multiple students with email : " + email);
+            }
+            if (students.isEmpty()) {
+                System.out.println("Inserting student : " + student);
+                repository.insert(student);
+            } else {
+                System.out.println(student + " already exists");
+            }
         };
     }
 }
